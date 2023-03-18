0e63e3bcd7f0503625ba0931b18e3563cd827dbf
Alekseenkov <kseenkovale@outlook.com>
2023-03-18 11:44:38 +0300

    mess

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-import "mycrypto/module"
+import (
+	"fmt"
+	"math/big"
+	"mycrypto/fftmy"
+)
 
 func main() {
 
-	module.InterfaceComparison1()
-	module.InterfaceComparison2()
-	module.InterfaceDiophantine()
-	module.InterfaceJacobi()
+	//module.InterfaceComparison1()
+	//module.InterfaceComparison2()
+	//module.InterfaceDiophantine()
+	//module.InterfaceJacobi()
+
+	var T = new(big.Int)
+	var E = new(big.Int)
+	var X = new(big.Int)
+
+	fmt.Println(" a*b = x ")
+
+	T = big.NewInt(5)
+	E = big.NewInt(5)
+
+	X = fftmy.MulFFT(T, E)
+
+	fmt.Println("x =", X)
 
 }
